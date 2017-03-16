137a8f5325600ba287cf8fadd0072ec8658b8062
LarissaMorrell <larissa@larissamorrell.com>
2017-03-16 14:15:09 -0400

    fixed syntax error in List.js, but now error - each child in array or iterator must have unique key prop

--- js/components/list.js
+++ js/components/list.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Card from './card';
 
 export default function List(props) {
-	
+    
     return (
-    	<div className="list">{props.title}</div>
-        <div className="list">{props.cards.map(function(cardText){
-        	return <Card text={cardText} />});
-        }</div>
+      <div>
+        <div className="list">{props.title}</div>
+          <div className="list">{props.cards.map(function(cardText){
+              return <Card text={cardText} />})
+          }
+        </div>
+      </div>
     );
 }
