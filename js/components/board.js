204b2f6f3a20833908e08822e033b4ccb7a17939
Lee B Brantley II <web_development_programmer@hotmail.com>
2017-03-19 10:32:18 -0500

    updated Board component

--- js/components/board.js
+++ js/components/board.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import List from './list';
 
-export default function Board(props) {
-    let lists = [];
-    let cards = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "Vivamus at molestie turpis. Nunc quam erat, commodo eu gravida nec.", "Ut et dapibus mi. Aliquam et erat nec lorem tempor pretium."];
-    let listTitles = props.lists
-    for (let i = 0; i < cards.length; i++) {
-        let titleStr = "This is list title " + (i + 1);
-        lists.push( 
-            <List title={listTitles[i]}
-                  cards={cards}/> );
-    }
-    console.log(lists);
+export default class Board extends React.Component {
 
-    return ( 
-        <div className="board">
-            <span>{props.title}</span>
-            {lists}
-        </div>
-    );
+    constructor(props) {
+        super(props);
+    }
+
+    render(){
+        let lists = [];
+        let cards = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit.", "Vivamus at molestie turpis. Nunc quam erat, commodo eu gravida nec.", "Ut et dapibus mi. Aliquam et erat nec lorem tempor pretium."];
+        let listTitles = props.lists
+        for (let i = 0; i < cards.length; i++) {
+            let titleStr = "This is list title " + (i + 1);
+            lists.push( 
+                <List title={listTitles[i]}
+                      cards={cards}/> );
+        }
+        console.log(lists);
+
+        return ( 
+            <div className="board">
+                <span>{props.title}</span>
+                {lists}
+            </div>
+        );
+    }
 }
